48a81f61db94a83e364d401813eb68b5b086f78c
Maria Paula Bastos <mapabastos@gmail.com>
2022-06-04 18:55:40 -0300

    teste 3

--- pages/api/teste_axios.js
+++ pages/api/teste_axios.js
@@ -1,14 +1,15 @@
 var file = 'C:\\Users\\Usuario\\Desktop\\1° período - ufrp\\formula\\sensores_do_chicote\\acelerometro\\teste_bd.csv';
 var file_2 = "https://docs.google.com/spreadsheets/d/1zvEt9-82MyFcREDqUVxHxOMyxREG_91ev2k0OzjfE00/export?format=csv"
-import * as fs from 'fs'
-import * as path from 'path'
+
 import axios from 'axios'
 
-var arquivo_csv =  fs.readFileSync(file, 'utf-8');
+//var arquivo_csv =  fs.readFileSync(file, 'utf-8');
 var teste = axios({
         method: 'get',
-        url: file_2
+        url: file_2,
+        responseType: 'stream'
     });
+
 console.log(teste);
 
 function Csvtojson(request, response){
