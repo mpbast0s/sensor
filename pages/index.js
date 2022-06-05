7ec493903dfdea85c5e959977f38920f2d389cd7
Maria Paula Bastos <mapabastos@gmail.com>
2022-06-04 23:42:25 -0300

    teste 10

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function Home() {
-    return <h1>teste2</h1>
+const url= "https://sensor-pied.vercel.app/api/csv_api";
+async function dados(){
+    const dados_fetch = await fetch('./api/csv_api');
+    const dados_json= await dados_fetch.json();
+    const dado = dados_json.ultimo_lido;
+    console.log(dado);
+    
+    return dado;
 }
+
+const dado = dados();
+console.log('-----');
+
+function Home() {
+    //const dado = dados();
+    return (
+        <div> <h1> teste 5 {dado}</h1></div>
+    )}
 
 export default Home
